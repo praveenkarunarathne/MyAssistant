1bf347a84cfc8504173328866ab07e7a3b21405e
praveen28624 <68008375+praveen28624@users.noreply.github.com>
2021-03-13 16:55:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import os
 app_id=2940667
 api_hash="8590c88aca3638eb321979577ddb53d3"
 id=869979136
-app = Client(
-    "my",app_id,api_hash)
+session_string ="BQCWP5HKSDNyjA5NpQRDgHZ92v4PRQBnWdxY07py9UkmMljHsmx_QTKlAm4AIjCh5O01rszuU4MV4uiPeunkuSe4hPQttLa6792-VnHdSv1K0uAaUntEGDkJj_2DSFMCyw5UAH5LtPJK1uw8zX71fIzDXqHeX_jxJXEl9zoGA70LxOUCbMhr_OY-oIMAiKwgulK3jh4w5nmxlxgm9lrAbRcelAQQRyOh53BLlF7q68jV0t3PyBy6kTXEC8j2IlQ_F9KHnUT51p6Jt0aCIiZ1r0AP8c_qzSBLLR-0q_xW5zdJaBjkgJLgMHzk0bi5fYfPcoHKY9YTjmEsNkdi1nyW1Wc0U-lWcgA"
+app = Client(session_string,app_id,api_hash)
 @app.on_message(filters.text & filters.private & ~filters.bot & ~filters.user(users=[id,1407800946]))
 def echo(client, message):
   f = open("uid.txt", "w")
