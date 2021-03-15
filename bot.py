95824ceb7a8dc5406ad760e3828836caba66947d
praveen28624 <68008375+praveen28624@users.noreply.github.com>
2021-03-15 08:27:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,10 +8,9 @@ translator = google_translator()
 
 app_id=2940667
 api_hash="8590c88aca3638eb321979577ddb53d3"
-id=869979136
 session_string ="BQBNf_r3IA5tLXdceJAc6qgbx5dKVyuismn8RMdqvbCo0wEr87lNw5ORvxSocM9MbOv2sXN8c6DNioL52-l1rW8TI9HXxP8M-DtkElj-j2R-1T04IhLGuf2pa3i73rJFjkt5cGSWP9hGX1NVx9G73lMWHKPpIbWyqb0z48t7_T5ji2VgIR3nuoSepV46klmNiz516--0K_wUEqAP0X1PaKQ-ke30HgpUi3b6uKIeNr27kibZphqBGT8cnQPgrps4WAuuf476l7blTYJM2lgqaQF0YXtOKA-vv2H0ZDiHeJYJl1sc2MurYMfPiCGmSahCIIkV-tIyH20BIJQ-Y_0sALE8U-lWcgA"
 app = Client(session_string,app_id,api_hash)
-@app.on_message(filters.text & filters.private & ~filters.bot & ~filters.user(users=[id,1407800946]))
+@app.on_message(filters.text & filters.private & ~filters.bot & ~filters.user(users=1407800946))
 def echo(client, message):
   message.reply_chat_action("typing")
   r = open("uid.txt", "a")
